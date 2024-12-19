f708c2ce0594e89bc9ade25f568596bf7d7fa2c9
hypermania <hypermania@uchicago.edu>
2024-12-19 20:57:33 +0800

    write to template

--- io.hpp
+++ io.hpp
@@ -5,7 +5,27 @@
 #include <vector>
 #include <iomanip>
 
+
+
+template<typename Scalar>
+void write_to_file(std::vector<Scalar> &vector, std::string filename){
+  char *memblock = (char *)&vector[0];
+  std::ofstream file(filename, std::ios::binary);
+  if(file.is_open()){
+    file.write(memblock, vector.size() * sizeof(Scalar));
+  }
+}
+
+#ifndef NOT_USING_EIGEN
+
 #include <Eigen/Dense>
+template<typename Derived>
+void write_to_file(const Eigen::PlainObjectBase<Derived> &obj, std::string filename){
+  std::ofstream file(filename, std::ios::binary);
+  if(file.is_open()){
+    file.write((char *)obj.data(), obj.size() * sizeof(typename Eigen::DenseBase<Derived>::Scalar));
+  }
+}
 
 template<typename Derived>
 void write_to_filename_template(const Eigen::PlainObjectBase<Derived> &obj, const std::string format_string, const int idx)
@@ -17,5 +37,7 @@ void write_to_filename_template(const Eigen::PlainObjectBase<Derived> &obj, cons
     file.write((char *)obj.data(), obj.size() * sizeof(typename Eigen::DenseBase<Derived>::Scalar));
   }
 }
+
+#endif
 
 #endif
